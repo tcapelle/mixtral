fce34d927c75e87e22d9aae72b8d616c3c97bde3
Thomas Capelle <tcapelle@pm.me>
2023-12-21 17:11:47 +0000

    make inference more custom

--- simple_inference.py
+++ simple_inference.py
@@ -1,14 +1,11 @@
-import os
+import time
 from types import SimpleNamespace
 
 import wandb
 
 import torch
-from transformers import AutoModelForCausalLM, AutoTokenizer, TrainingArguments
-from trl import SFTTrainer
+from transformers import AutoModelForCausalLM, AutoTokenizer
 from datasets import load_dataset
-from peft import LoraConfig, get_peft_model
-
 from utils import parse_args
 
 WANDB_PROJECT = "mixtral"
@@ -16,9 +13,12 @@ WANDB_ENTITY = "capecape"
 
 config = SimpleNamespace(
     model_id = "mistralai/Mixtral-8x7B-v0.1",
-    batch_size = 1, 
+    text = "Tell me a story about planes and cats, make it Tolkien-esque: ",
     use_flash_attention_2=False,
-    load_in_4bit=True,
+    load_in_4bit=False,
+    load_in_8bit=False,
+    use_cache=True,
+    max_new_tokens=100,
 )
 
 
@@ -28,14 +28,19 @@ def main(config):
     model = AutoModelForCausalLM.from_pretrained(
         config.model_id, 
         use_flash_attention_2=config.use_flash_attention_2,
-        load_in_4bit=config.load_in_4bit,
+        load_in_4bit=config.load_in_4bit and not config.load_in_8bit,
+        load_in_8bit=config.load_in_8bit and not config.load_in_4bit,
+        use_cache=config.use_cache,
+        device_map="auto",
     )
 
-    text = "Tell me a story about planes and cats, make it Tolkien-esque: "
-    inputs = tokenizer(text, return_tensors="pt").to(0)
-
-    outputs = model.generate(**inputs, max_new_tokens=100)
+    
+    inputs = tokenizer(config.text, return_tensors="pt").to(0)
+    t = time.perf_counter()
+    outputs = model.generate(**inputs, max_new_tokens=config.max_new_tokens)
+    tf = time.perf_counter() - t
     print(tokenizer.decode(outputs[0], skip_special_tokens=True))
+    print(f"Total Tokens {len(outputs[0])}\nTime taken: {tf:.2f}s\ntokens/s: {len(outputs[0])/tf:.2f}")
 
 if __name__ == "__main__":
     parse_args(config)
