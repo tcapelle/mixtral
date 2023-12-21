314b256b313419d8a1b4939501601e3f8b97749c
Thomas Capelle <tcapelle@pm.me>
2023-12-21 15:05:08 +0000

    add utils

--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
 import argparse
 from ast import literal_eval
 
+from transformers import Trainer
+
 def str2bool(v):
     "Fix Argparse to process bools"
     if isinstance(v, bool):
@@ -31,3 +33,48 @@ def parse_args(config):
             attempt = v
         setattr(config, k, attempt)
         print(f"--{k}:{v}")
+
+
+def debug_trainer_data(trainer: Trainer):
+    """Print a bunch of debug info about how the packed dataset is being constructed.
+    We set everythin to finite to avoid iterating forever"""
+    print("Computing Dataset Stats...")
+    train_ds = trainer.train_dataset
+    len_train_ds = sum(1 for _ in train_ds)
+    print(
+        f"  len(train_ds): {len_train_ds}\n"
+    )
+    train_dl = trainer.get_train_dataloader()
+    train_dl.dataset.infinite = False
+    len_train_dl = sum(1 for _ in train_dl)
+    b = next(iter(train_dl))
+    input_ids, labels = b["input_ids"], b["labels"]
+    
+    print(
+        f"  len(train_dl): {len_train_dl}\n"
+        f"  batch_shape  : {input_ids.shape}\n"
+    )
+    tokenizer = trainer.tokenizer
+    decoded_ids = tokenizer.decode(input_ids[0])[0:80]
+    decoded_labels = tokenizer.decode(labels[0])[0:80]
+    print("First batch:\n"
+          f"input_ids:\n{decoded_ids}\n"
+          f"labels:\n{decoded_labels}\n")
+
+
+def _prompt_no_input(row):
+    return ("Below is an instruction that describes a task. "
+            "Write a response that appropriately completes the request.\n\n"
+            "### Instruction:\n{instruction}\n\n### Response:\n").format_map(row)
+
+def _prompt_input(row):
+    return ("Below is an instruction that describes a task, paired with an input that provides further context. "
+            "Write a response that appropriately completes the request.\n\n"
+            "### Instruction:\n{instruction}\n\n### Input:\n{input}\n\n### Response:\n").format_map(row)
+
+def create_alpaca_prompt(row):
+    return _prompt_no_input(row) if row["input"] == "" else _prompt_input(row)
+
+def create_alpaca_prompt_with_response(row):
+    instruct = _prompt_no_input(row) if row["input"] == "" else _prompt_input(row)
+    return instruct + row["output"]
